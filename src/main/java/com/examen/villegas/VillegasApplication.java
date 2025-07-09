268720ca8266fd9836e73eac4e696b5f72373114
nsvillegas@espe.edu.ec <nsvillegas@espe.edu.ec>
2025-07-09 17:39:22 -0500

    Creacion-ProyectoExamP2

---
+++ src/main/java/com/examen/villegas/VillegasApplication.java
@@ -0,0 +1,13 @@
+package com.examen.villegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillegasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/villegas/VillegasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.villegas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
